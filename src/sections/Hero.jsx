e6b2c8991ddc8f52843d23c79a3a0f8208ed68f6
Raamishshah010 <raamishshah010@gmail.com>
2023-08-27 15:11:36 +0500

    Working On Hero Section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,50 @@
+import { arrowRight } from "../assets/icons"
+import { bigShoe1 } from "../assets/images"
+import Button from "../components/Button"
+import { statistics } from "../constants"
 
-    const Hero = () => {
+const Hero = () => {
     return (
-        <div>Hero</div>
-    )
-    }
+        <section
+            id="home"
+            className="max-xlg:w-screen flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container "
+        >
 
-    export default Hero
+            <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-lg:padding-x pt-28">
+
+                <p className="text-xl font-montserrat text-coral-red">Our Summer Collections</p>
+                <h1 className="mt-10 font-palanquin text-8xl max-sm:font-[42px] max-sm:leading-[82] font-bold">
+                    <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10 ">The New Arrival</span>
+                    <br />
+                    <span className="text-coral-red inline-block mt-3">Nike </span> Shoes
+
+                </h1>
+                <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish Nike arrivals, quality comfort, and innovation for
+                    your active life. </p>
+
+                <Button label="Shop Now" iconUrl={arrowRight} />
+
+                <div className="flex justify-start items-start flex-wrap mt-20 gap-16 w-full">
+                    {
+                        statistics.map((stats) => (
+                            <div key={stats.label}>
+                                <p className="text-4xl font-palanquin font-bold">{stats.value}</p>
+                                <p className="font-montserrat leading-6 text-slate-gray">{stats.label}</p>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+
+
+            {/* HERO IMAGE */}
+
+            <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-center bg-cover">
+                <img src={bigShoe1} alt="Hero Shoe Image" width={610} height={500} className="object-contain z-10" />
+            </div>
+
+        </section>
+    )
+}
+
+export default Hero
