50209cea0f532ea082eb8a0ec1f890dbfbfc57de
Raamishshah010 <raamishshah010@gmail.com>
2023-08-28 22:35:31 +0500

    Working on footer  section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -19,8 +19,8 @@ const Hero = () => {
             <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-lg:padding-x pt-28">
 
                 <p className="text-xl font-montserrat text-coral-red">Our Summer Collections</p>
-                <h1 className="mt-10 font-palanquin text-8xl max-sm:font-[42px] max-sm:leading-[1] font-bold">
-                    <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10 ">The New Arrival</span>
+                <h1 className="mt-10 font-palanquin text-8xl max-sm:font-[42px] max-sm:leading-[1] font-bold  max-sm:text-6xl ">
+                    <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
                     <br />
                     <span className="text-coral-red inline-block mt-3">Nike </span> Shoes
 
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,7 +1,33 @@
 
 const Subscribe = () => {
     return (
-        <div>Subscribe</div>
+        <section id="contact-us" className="
+                max-container 
+                flex
+                flex-col
+                justify-center
+                items-center
+                max-lg:flex-col
+                gap-10
+                
+                ">
+
+
+            <h3 className="text-4xl leading-4 font-palanquin font-semibold text-slate-900 max-md:text-4xl">Sign Up For <span
+            className="text-coral-red"
+            >
+                    Updates 
+                </span>  & Newsletters
+            </h3>
+
+            <div className='flex border-2 w-[60%] mt-5 rounded-xl relative'>
+                <input type="text" placeholder="subscribe@nike.com"
+                    className="flex-1 sm:flex-1 max-sm: w-full text-base leading-normal text-slate-gray pl-5 max-sm: p-5 outline-none sm:border-none border max-sm:border-slate-gray max-sm: rounded-full"
+                />
+                <button className="py-4 bg-coral-red rounded-2xl px-12 text-white absolute right-[-40px] top-1">Sign Up</button>
+            </div>
+            
+        </section>
     )
 }
 
